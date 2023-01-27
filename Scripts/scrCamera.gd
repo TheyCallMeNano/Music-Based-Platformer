0d6ea1584443d27c776c03bec845cd477102daf9
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2023-01-27 13:56:12 -0600

    Fix the improper zoom ratio on the camera

--- Scripts/scrCamera.gd
+++ Scripts/scrCamera.gd
@@ -3,11 +3,11 @@ extends Camera2D
 func _process(delta):
 	if get_parent().motion.x > 300:
 		zoom.x += .00005
-		zoom.y += .00002
+		zoom.y += .00005
 	elif get_parent().motion.x < get_parent().speed:
 		if zoom.x != 0.4:
 			zoom.x -= 0.0005
-			zoom.y -= 0.0002
+			zoom.y -= 0.0005
 		if zoom.x < 0.4:
 			zoom.x = 0.4
 			zoom.y = 0.4
