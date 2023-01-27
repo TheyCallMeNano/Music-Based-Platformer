de3a83918fdd95590bf5306f3a186afe0422f136
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2023-01-27 16:54:52 -0600

    Grappling Hook and Camera Fixes

--- Scripts/scrCamera.gd
+++ Scripts/scrCamera.gd
@@ -1,13 +1,14 @@
 extends Camera2D
 
 func _process(delta):
-	if get_parent().motion.x > 300:
-		zoom.x += .00005
-		zoom.y += .00005
-	elif get_parent().motion.x < get_parent().speed:
+	if get_parent().motion.y > 300 || get_parent().motion.x > 300 || get_parent().motion.x < -10:
+		if zoom.x < 0.6:
+			zoom.x += .0005
+			zoom.y += .0005
+	elif get_parent().motion.y < get_parent().speed || get_parent().motion.x < get_parent().speed:
 		if zoom.x != 0.4:
-			zoom.x -= 0.0005
-			zoom.y -= 0.0005
+			zoom.x -= 0.003
+			zoom.y -= 0.003
 		if zoom.x < 0.4:
 			zoom.x = 0.4
 			zoom.y = 0.4
--- Scripts/scrPlayer.gd
+++ Scripts/scrPlayer.gd
@@ -10,6 +10,14 @@ const UP = Vector2(0,-1)
 
 var motion = Vector2()
 
+var hookPos = Vector2()
+var hooked = false
+var ropeLength = 500
+var currentRopeLength
+
+func _ready():
+	currentRopeLength = ropeLength
+
 func _physics_process(delta):
 	motion.y += grav
 	
@@ -30,6 +38,26 @@ func _physics_process(delta):
 			motion.y = -jump
 		
 	motion = move_and_slide(motion,UP)
+	
+	hook()
+	update()
+	if hooked:
+		motion.y += grav
+		hookSwing(delta)
+		#Swing Speed
+		motion *= 0.9
+		motion = move_and_slide(motion)
+
+func _draw():
+	var pos = global_position
+	if hooked:
+		draw_line(Vector2(0,-1), to_local(hookPos), Color(0.35, 0.7, 0.9), 3, true)
+	else:
+		return
+		var colliding = $Raycast.is_colliding()
+		var collidePoint = $Raycast.get_collision_point()
+		if colliding && pos.distance_to(collidePoint):
+			draw_line(Vector2(0,-1), to_local(collidePoint), Color(1,1,1), 0.5, true)
 
 func _process(delta):
 	if Input.is_action_just_pressed("slot1"):
@@ -38,3 +66,31 @@ func _process(delta):
 		global.equipped = [0,1,0]
 	if Input.is_action_just_pressed("slot3"):
 		global.equipped = [0,0,1]
+
+func hook():
+	$Raycast.look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("primaryFire"):
+		hookPos = getHookPos()
+		if hookPos:
+			hooked = true
+			currentRopeLength = global_position.distance_to(hookPos)
+	if Input.is_action_just_released("primaryFire") && hooked:
+		hooked = false
+
+func getHookPos():
+	for raycast in $Raycast.get_children():
+		if raycast.is_colliding():
+			return raycast.get_collision_point()
+
+func hookSwing(delta):
+	var radius = global_position - hookPos
+	if motion.length() < 0.01 or radius.length() < 10:
+		return
+	var angle = acos(radius.dot(motion) / (radius.length() * motion.length()))
+	var radVel = cos(angle) * motion.length()
+	motion += radius.normalized() * -radVel
+	
+	if global_position.distance_to(hookPos) > currentRopeLength:
+		global_position = hookPos + radius.normalized() * currentRopeLength
+	
+	motion += (hookPos - global_position).normalized() * 15000 * delta
