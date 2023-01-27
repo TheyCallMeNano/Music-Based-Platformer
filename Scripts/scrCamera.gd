7c13d374007dd2d3d37d6dd28f909a4570ef8dea
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2023-01-27 17:43:22 -0600

    Camera Hotfix

--- Scripts/scrCamera.gd
+++ Scripts/scrCamera.gd
@@ -3,12 +3,12 @@ extends Camera2D
 func _process(delta):
 	if get_parent().motion.y > 300 || get_parent().motion.x > 300 || get_parent().motion.x < -10:
 		if zoom.x < 0.6:
-			zoom.x += .0005
-			zoom.y += .0005
+			zoom.x += .0003
+			zoom.y += .0003
 	elif get_parent().motion.y < get_parent().speed || get_parent().motion.x < get_parent().speed:
 		if zoom.x != 0.4:
-			zoom.x -= 0.003
-			zoom.y -= 0.003
+			zoom.x -= 0.0001
+			zoom.y -= 0.0001
 		if zoom.x < 0.4:
 			zoom.x = 0.4
 			zoom.y = 0.4
