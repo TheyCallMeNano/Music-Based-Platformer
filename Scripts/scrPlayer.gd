d4bfa40d9f74c634e9d4feedbbbab5195a719dc2
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2023-01-27 13:02:12 -0600

    Movement and Momentum

---
+++ Scripts/scrPlayer.gd
@@ -0,0 +1,32 @@
+#This is essentially what we are modifiying
+extends KinematicBody2D
+
+const speed = 800
+const jump = -200
+const grav = 8
+const accl = 50
+
+const UP = Vector2(0,-1)
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += grav
+	
+	if Input.is_action_pressed("moveRight"):
+		motion.x = min(motion.x+accl,speed)
+	
+	elif Input.is_action_pressed("moveLeft"):
+		motion.x = max(motion.x-accl,-speed)
+	
+	else:
+		motion.x = lerp(motion.x,0,0.2)
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("moveUp"):
+			motion.y = jump
+	elif !is_on_floor():
+		if Input.is_action_just_pressed("moveDown"):
+			motion.y = -jump
+		
+	motion = move_and_slide(motion,UP)
