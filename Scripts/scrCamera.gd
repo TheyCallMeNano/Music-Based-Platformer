6339b6bf6929a26635d2c322e3d72e53857c9f18
TheyCallMeNano <xnanoxplaysmc@gmail.com>
2023-01-27 13:52:43 -0600

    Fixed m00tY's bullshit

---
+++ Scripts/scrCamera.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+func _process(delta):
+	if get_parent().motion.x > 300:
+		zoom.x += .00005
+		zoom.y += .00002
+	elif get_parent().motion.x < get_parent().speed:
+		if zoom.x != 0.4:
+			zoom.x -= 0.0005
+			zoom.y -= 0.0002
+		if zoom.x < 0.4:
+			zoom.x = 0.4
+			zoom.y = 0.4
