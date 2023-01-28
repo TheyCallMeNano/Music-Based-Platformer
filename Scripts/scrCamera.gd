4133bebd69094655fe03f6cfb9a6ab02d815652b
Zac <zacrex@outlook.com>
2023-01-27 20:30:46 -0500

    updated camera zoom rates

--- Scripts/scrCamera.gd
+++ Scripts/scrCamera.gd
@@ -1,14 +1,14 @@
 extends Camera2D
 
-func _process(delta):
+func _process(_delta):
 	if get_parent().motion.y > 300 || get_parent().motion.x > 300 || get_parent().motion.x < -10:
 		if zoom.x < 0.6:
-			zoom.x += .0003
-			zoom.y += .0003
+			zoom.x += .0005
+			zoom.y += .0005
 	elif get_parent().motion.y < get_parent().speed || get_parent().motion.x < get_parent().speed:
 		if zoom.x != 0.4:
-			zoom.x -= 0.0001
-			zoom.y -= 0.0001
+			zoom.x -= .00025
+			zoom.y -= .00025
 		if zoom.x < 0.4:
 			zoom.x = 0.4
 			zoom.y = 0.4
