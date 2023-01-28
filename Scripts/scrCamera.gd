78b190a5b1eca3917d52aefb482adf2baf5e98ed
Zac <zacrex@outlook.com>
2023-01-27 22:23:18 -0500

    Update scrCamera.gd

--- Scripts/scrCamera.gd
+++ Scripts/scrCamera.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 func _process(_delta):
 	if get_parent().motion.y > 300 || get_parent().motion.x > 300 || get_parent().motion.x < -10:
-		if zoom.x < 0.6:
+		if zoom.x < 0.5:
 			zoom.x += .0005
 			zoom.y += .0005
 	elif get_parent().motion.y < get_parent().speed || get_parent().motion.x < get_parent().speed:
